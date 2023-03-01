cf6cd0ac67552e794358c3b3898afdad55da477a
mb <maarten2007.buelens@gmail.com>
2023-03-01 16:05:53 +0000

    removed prints and added returns for functions

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 setup(
     name='schoolware_api',
     packages=find_packages(include=['schoolware_api']),
-    version='0.1.0',
+    version='0.0.2',
     description='a schoolware api made in python',
     author='Me',
     license='MIT',
-    install_requires=['requests==2.25.1'],
+    install_requires=['requests==2.25.1', 'playwright==1.31.1'],
 )
