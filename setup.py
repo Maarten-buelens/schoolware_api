589ccbd4ca94a6eeb57ff5f8070545f62ac4f744
maarten-buelens <maarten2007.buelens@gmail.com>
2023-04-28 11:09:19 +0000

    version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='schoolware_api',
     packages=find_packages(include=['schoolware_api']),
-    version='1.0.7',
+    version='1.0.8',
     description='a schoolware api made in python',
     author='Maarten Buelens',
     license='MIT',
